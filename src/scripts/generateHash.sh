2bfaaa16d5235281d58a1c3fd7f75cfa1f61f880
Deep Bhatt <dbhatt@pointmotion.us>
2022-03-31 19:45:45 +0530

    add info comment

--- src/scripts/generateHash.sh
+++ src/scripts/generateHash.sh
@@ -3,5 +3,9 @@
 # Use this script to generate users' password's hash before inserting to the database.
 
 # Usage: ./generateHash.sh <password>
+
+# To run it as a npm script
+# Usage: npm run generate-hash -- <password>
+
 hash="$(htpasswd -bnBC 10 "" $1 | tr -d ':\n' | sed 's/$2y/$2b/')";
 echo $hash;
