8d53072c0e70e97e7d7cfe7488df5af94ee3f6f8
Deep Bhatt <dbhatt@pointmotion.us>
2022-06-02 13:42:24 +0530

    fix: 🐛 a better response struct for Monthly Goals API

--- src/patient/stats/stats.controller.ts
+++ src/patient/stats/stats.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, HttpCode, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { User } from 'src/auth/decorators/user.decorator';
 import { Role } from 'src/auth/enums/role.enum';
@@ -10,43 +19,42 @@ export class StatsController {
   @Get('monthly-goals/:month')
   @Roles(Role.PATIENT)
   @UseGuards(AuthGuard, RolesGuard)
+  @ApiBearerAuth('access-token')
   async monthyGoals(@Param('month') month: number, @User() userId: string) {
     console.log('userId:', userId);
+
     const monthyGoals = [
       {
-        day: 1,
-        sessions: [{ id: 'b8c59e0d-fcc6-43f1-a900-fff554dc60d9', status: 'completed' }],
+        date: '2022-05-01',
+        totalSessionDurationInMin: 30,
       },
       {
-        day: 2,
-        sessions: [{ id: 'ec623a4a-a373-49b9-ab54-8848eb8a1a98', status: 'partiallycompleted' }],
+        date: '2022-05-02',
+        totalSessionDurationInMin: 25,
       },
       {
-        day: 3,
-        sessions: [
-          { id: '4fd24aca-435d-4989-b264-c7bfd505780c', status: 'completed' },
-          { id: '989a4baa-4146-45ec-8800-3c5a55e8e654', status: 'partiallycompleted' },
-        ],
+        date: '2022-05-03',
+        totalSessionDurationInMin: 45,
       },
       {
-        day: 4,
-        sessions: [
-          { id: '71bba372-0ae8-44ba-8f50-82c31d14bf74', status: 'completed' },
-          { id: '0901b9ab-f96d-4248-b9a8-0f24333c6d83', status: 'partiallycompleted' },
-          { id: 'd464462a-2f76-4d4b-8b3b-9d8a13ae1423', status: 'exceeded' },
-        ],
+        date: '2022-05-04',
+        totalSessionDurationInMin: 39,
       },
       {
-        day: 5,
-        sessions: [{ id: '7d98cd54-0111-4211-82a1-e3ec9e88f76a', status: 'partiallycompleted' }],
+        date: '2022-05-07',
+        totalSessionDurationInMin: 35,
       },
       {
-        day: 6,
-        sessions: [],
+        date: '2022-05-08',
+        totalSessionDurationInMin: 10,
       },
       {
-        day: 7,
-        sessions: [{ id: '6605fd16-477a-4a69-ba53-22318a7262ef', status: 'completed' }],
+        date: '2022-05-09',
+        totalSessionDurationInMin: 36,
+      },
+      {
+        date: '2022-05-10',
+        totalSessionDurationInMin: 10,
       },
     ];
 
@@ -56,11 +64,19 @@ export class StatsController {
   }
 
   @HttpCode(200)
-  @Get('daily-goals/:day')
+  @Get('daily-goals/:date')
   @Roles(Role.PATIENT)
   @UseGuards(AuthGuard, RolesGuard)
-  async dailyGoals(@Param('day') day: number, @User() userId: string) {
+  @ApiBearerAuth('access-token')
+  async dailyGoals(@Param('date') date: string, @User() userId: string) {
     // returns the number of minutes a patient did a session on said day. can be >30min
+    const dailyGoalDate = new Date(date);
+    if (dailyGoalDate.toString() === 'Invalid Date') {
+      throw new HttpException('Invalid Date', HttpStatus.BAD_REQUEST);
+    }
+
+    let oneDayInFuture = new Date(date);
+    oneDayInFuture = new Date(oneDayInFuture.setDate(dailyGoalDate.getDate() + 1));
 
     // steps:
     // fetch all the sessions created for the day
