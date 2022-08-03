c002189563945f323d0afe6e2a128209102c25c6
Deep Bhatt <dbhatt@pointmotion.us>
2022-08-03 11:26:36 +0530

    refactor: 💡 clean up cron jobs

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,72 +1,11 @@
-import { Body, Controller, HttpCode, Logger, Post } from '@nestjs/common';
-import { EventsService } from 'src/events/events.service';
-import {
-  ScheduleEmailFeedback,
-  SessionEventTriggerRequestDto,
-  SessionInspectorEvent,
-} from './cron.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ScheduleEmailFeedback } from './cron.dto';
 import { CronService } from './cron.service';
 @Controller('cron')
 // TODO: Enable Guards later.
 // @UseGuards(AuthGuard)
 export class CronController {
-  private readonly logger = new Logger(CronController.name);
-
-  constructor(private cronService: CronService, private eventsService: EventsService) {}
-
-  @Post('schedule-session-inspector')
-  @HttpCode(201)
-  // called when a session is inserted.
-  async scheduleSessionInspector(@Body() body: SessionEventTriggerRequestDto) {
-    const now = new Date();
-    const fourtyFiveMinsInFuture = new Date(now.getTime() + 1000 * 60 * 45).toISOString();
-    const { sessionId, createdAt, patientId } = body;
-
-    const payload = {
-      sessionId,
-      createdAt,
-      patientId,
-    };
-
-    await this.cronService.scheduleOneOffCron(
-      fourtyFiveMinsInFuture,
-      '/cron/session/inspection',
-      payload,
-    );
-
-    return {
-      status: 'success',
-    };
-  }
-
-  @Post('session/inspection')
-  @HttpCode(200)
-  // runs at 45mins after a session has been created.
-  async inspectSessions(@Body() body: SessionInspectorEvent) {
-    // The cronjob will run after 45min a session has been inserted.
-    // Mark session as 'trashed', if no `taskEnded` event is found.
-    // Mark session as 'completed' if session lasted for at least 30 minutes.
-    // Mark session as 'partiallycompleted' if session lasted for less than 30 minutes.
-
-    const { sessionId, createdAt } = body.payload;
-    this.logger.debug('inspectSessions:sessionId:', sessionId);
-    this.logger.debug('inspectSessions:createdAt:', createdAt);
-
-    const sessionCreatedAt = new Date(createdAt);
-    const nowDate = new Date();
-    const diffInMins = (nowDate.getTime() - sessionCreatedAt.getTime()) / 1000 / 60;
-
-    // only run if difference is greater or equal to 45
-    if (diffInMins < 45) {
-      return;
-    }
-
-    await this.cronService.inspectSession(sessionId);
-
-    return {
-      status: 'success',
-    };
-  }
+  constructor(private cronService: CronService) {}
 
   @Post('email-patient-feedback')
   async scheduleEmailFeedback(@Body() body: ScheduleEmailFeedback) {
--- src/cron/cron.dto.ts
+++ src/cron/cron.dto.ts
@@ -1,38 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-
-export class SessionEventTriggerRequestDto {
-  @ApiProperty({
-    description: 'Session ID for which to schedule the inspection.',
-  })
-  @IsNotEmpty()
-  sessionId: string;
-
-  @ApiProperty({
-    description: 'Session creation date.',
-  })
-  @IsNotEmpty()
-  createdAt: Date;
-
-  @ApiProperty({
-    description: 'Patient ID associated with the session.',
-  })
-  @IsNotEmpty()
-  patientId: string;
-}
-
-export class SessionInspectorEvent {
-  @ApiProperty({
-    description: 'Session payload for which to schedule the inspection.',
-  })
-  @IsNotEmpty()
-  payload: {
-    sessionId: string;
-    createdAt: Date;
-  };
-  comment: string;
-}
-
 export class ScheduleEmailFeedback {
   @ApiProperty({
     description: 'Feedback ID to be scheduled for email delivery',
