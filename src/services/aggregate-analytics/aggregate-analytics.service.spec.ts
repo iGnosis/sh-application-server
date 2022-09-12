275465918bb6e4789689090424ecf4f0e4e003aa
Deep Bhatt <dbhatt@pointmotion.us>
2022-09-12 18:29:27 +0530

    test: 💍 aggregation service test

--- src/services/aggregate-analytics/aggregate-analytics.service.spec.ts
+++ src/services/aggregate-analytics/aggregate-analytics.service.spec.ts
@@ -2,9 +2,52 @@ import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { GqlService } from '../gql/gql.service';
 import { AggregateAnalyticsService } from './aggregate-analytics.service';
+import { AnalyticsDTO, Sit2StandAnalyticsDTO } from 'src/types/analytics';
 
 describe('AggregateAnalyticsService', () => {
   let service: AggregateAnalyticsService;
+  const testData: AnalyticsDTO[] = [
+    {
+      prompt: {
+        type: 'x',
+        timestamp: 123,
+        data: {
+          number: 1,
+        },
+      },
+      result: {
+        type: 'success',
+        timestamp: 1,
+        score: 1,
+      },
+      reaction: {
+        type: 'x',
+        timestamp: 1,
+        startTime: new Date().getTime(),
+        completionTime: 10,
+      },
+    },
+    {
+      prompt: {
+        type: 'x',
+        timestamp: 123,
+        data: {
+          number: 1,
+        },
+      },
+      result: {
+        type: 'failure',
+        timestamp: 1,
+        score: 0,
+      },
+      reaction: {
+        type: 'x',
+        timestamp: 1,
+        startTime: new Date().getTime(),
+        completionTime: 20,
+      },
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,5 +59,21 @@ describe('AggregateAnalyticsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should calculate avg achievement ratio', () => {
+    expect(service.averageAchievementRatio(testData)).toEqual({
+      key: 'avgAchievementRatio',
+      value: 0.5,
+      noOfSamples: 2,
+    });
+  });
+
+  it('should calculate avg completion ratio', () => {
+    expect(service.averageCompletionRatio(testData)).toEqual({
+      key: 'avgCompletionTime',
+      value: 15,
+      noOfSamples: 2,
+    });
   });
 });
