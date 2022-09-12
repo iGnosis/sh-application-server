04d1ef25dd40447abf0fd2b6438e7e80ef6bc3a2
Deep Bhatt <dbhatt@pointmotion.us>
2022-09-12 17:39:25 +0530

    refactor: 💡 put test in its describe block

--- src/services/extract-information/extract-information.service.spec.ts
+++ src/services/extract-information/extract-information.service.spec.ts
@@ -12,32 +12,34 @@ describe('ExtractInformationService', () => {
     service = module.get<ExtractInformationService>(ExtractInformationService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  describe('median of numbers', () => {
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+    });
 
-  it('should calculate the median of odd-numbered dataset', () => {
-    const result = service.median([5, 6, 50, 1, -5]);
-    expect(result).toEqual(5);
-  });
+    it('should calculate the median of odd-numbered dataset', () => {
+      const result = service.median([5, 6, 50, 1, -5]);
+      expect(result).toEqual(5);
+    });
 
-  it('should consider only first 3 numbers', () => {
-    const result = service.median([5, 6, 50, 1, -5], 3);
-    expect(result).toEqual(6);
-  });
+    it('should consider only first 3 numbers', () => {
+      const result = service.median([5, 6, 50, 1, -5], 3);
+      expect(result).toEqual(6);
+    });
 
-  it('should calculate the median of odd-numbered dataset', () => {
-    const result = service.median([5, 6, 50, 1, -5, 12]);
-    expect(result).toEqual(5.5);
-  });
+    it('should calculate the median of odd-numbered dataset', () => {
+      const result = service.median([5, 6, 50, 1, -5, 12]);
+      expect(result).toEqual(5.5);
+    });
 
-  it('should handle two numbers in a dataset', () => {
-    const result = service.median([5, 6, 50, 1, -5, 12], 2);
-    expect(result).toEqual(5.5);
-  });
+    it('should handle two numbers in a dataset', () => {
+      const result = service.median([5, 6, 50, 1, -5, 12], 2);
+      expect(result).toEqual(5.5);
+    });
 
-  it('should handle empty dataset', () => {
-    const result = service.median([]);
-    expect(result).toBeFalsy();
+    it('should handle empty dataset', () => {
+      const result = service.median([]);
+      expect(result).toBeFalsy();
+    });
   });
 });
