863c94e91a1154f3e2bc7db15eb2e71a95fdcc71
Deep Bhatt <dbhatt@pointmotion.us>
2022-12-09 17:13:53 +0530

    fix: 🐛 pass failing test

--- src/services/stripe/stripe.service.spec.ts
+++ src/services/stripe/stripe.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { StripeService } from './stripe.service';
 
@@ -6,7 +7,7 @@ describe('StripeService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StripeService],
+      providers: [StripeService, ConfigService],
     }).compile();
 
     service = module.get<StripeService>(StripeService);
