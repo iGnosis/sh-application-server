f02cb3f545cc975bc9092478bdcb573cc8f0b90b
Mohan Vaddi <myselfgeek01@gmail.com>
2022-12-13 18:28:45 +0530

    fix: 🐛 fix failing test cases

--- src/controllers/payment/patient-payment/patient-payment.controller.spec.ts
+++ src/controllers/payment/patient-payment/patient-payment.controller.spec.ts
@@ -1,4 +1,7 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { GqlService } from 'src/services/clients/gql/gql.service';
+import { StripeService } from 'src/services/stripe/stripe.service';
 import { PatientPaymentController } from './patient-payment.controller';
 
 describe('PatientPaymentController', () => {
@@ -7,6 +10,7 @@ describe('PatientPaymentController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientPaymentController],
+      providers: [StripeService, ConfigService, GqlService],
     }).compile();
 
     controller = module.get<PatientPaymentController>(PatientPaymentController);
