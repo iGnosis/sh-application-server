8703dbd5ab400f573c4fff3ffd82ff6ad6f8591b
Deep Bhatt <dbhatt@pointmotion.us>
2022-12-14 14:00:18 +0530

    fix: 🐛 pass failing test

--- src/mock/mock.controller.spec.ts
+++ src/mock/mock.controller.spec.ts
@@ -1,4 +1,10 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { EmailService } from 'src/services/clients/email/email.service';
+import { GqlService } from 'src/services/clients/gql/gql.service';
+import { SmsService } from 'src/services/clients/sms/sms.service';
+import { SmsAuthService } from 'src/services/sms-auth/sms-auth.service';
 import { MockController } from './mock.controller';
 
 describe('MockController', () => {
@@ -7,6 +13,7 @@ describe('MockController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MockController],
+      providers: [ConfigService, SmsAuthService, GqlService, SmsService, EmailService, Logger],
     }).compile();
 
     controller = module.get<MockController>(MockController);
