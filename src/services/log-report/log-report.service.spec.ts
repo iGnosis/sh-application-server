340a3e9409edc3f0efaaa7633e7cc45148397675
vigneshrajj <vigneshhrajj@gmail.com>
2023-02-13 20:50:06 +0530

    test: 💍 fix failing tests

--- src/services/log-report/log-report.service.spec.ts
+++ src/services/log-report/log-report.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogReportService } from './log-report.service';
 
@@ -6,7 +7,7 @@ describe('LogReportService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LogReportService],
+      providers: [LogReportService, ConfigService],
     }).compile();
 
     service = module.get<LogReportService>(LogReportService);
