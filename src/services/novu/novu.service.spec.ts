c7dbadab646389e72ea47efa92d7eb7ca0b25c8e
Deep Bhatt <dbhatt@pointmotion.us>
2023-03-09 14:09:32 +0530

    fix: 🐛 remove tests that depends on network call

--- src/services/novu/novu.service.spec.ts
+++ src/services/novu/novu.service.spec.ts
@@ -16,26 +16,4 @@ describe('NovuService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-
-  it('should update paymentMade to true', async () => {
-    const patientId = '542476cb-4e94-46d9-987f-b01f0679054b';
-    const resp = await service.novuClient.subscribers.update(patientId, {
-      data: {
-        paymentMade: true,
-      },
-    });
-    console.log(resp.data);
-    expect(resp).toBeTruthy();
-  });
-
-  it('should update paymentMade to false', async () => {
-    const patientId = '542476cb-4e94-46d9-987f-b01f0679054b';
-    const resp = await service.novuClient.subscribers.update(patientId, {
-      data: {
-        paymentMade: false,
-      },
-    });
-    console.log(resp);
-    expect(resp).toBeTruthy();
-  });
 });
