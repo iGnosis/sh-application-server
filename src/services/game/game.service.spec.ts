4eacf9abf106a149088f4fe53269e9541a16660b
Deep Bhatt <dbhatt@pointmotion.us>
2023-04-10 13:07:35 +0530

    fix: failing test

--- src/services/game/game.service.spec.ts
+++ src/services/game/game.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameService } from './game.service';
+import { GqlService } from '../clients/gql/gql.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('GameService', () => {
   let service: GameService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GameService],
+      providers: [GameService, GqlService, ConfigService],
     }).compile();
 
     service = module.get<GameService>(GameService);
