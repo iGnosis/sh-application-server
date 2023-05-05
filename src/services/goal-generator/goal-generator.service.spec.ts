4ad90299c3a81826af815fc79940c5152b94060c
Deep Bhatt <dbhatt@pointmotion.us>
2023-05-05 12:48:23 +0530

    refactor: 💡 remove unused imports

--- src/services/goal-generator/goal-generator.service.spec.ts
+++ src/services/goal-generator/goal-generator.service.spec.ts
@@ -1,7 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GoalGeneratorService } from './goal-generator.service';
-import { Badge, Goal, PatientBadge, UserContext } from 'src/types/global';
-import { GoalStatus, Metrics } from 'src/types/enum';
 import { GqlService } from '../clients/gql/gql.service';
 import { ConfigService } from '@nestjs/config';
 import { StatsService } from '../patient-stats/stats.service';
@@ -22,40 +20,5 @@ describe('GoalGeneratorService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  xit('should generate goals', async () => {
-    const patientId = '111';
-    // Given
-    const userContext: UserContext = {
-      PATIENT_STREAK: 7,
-    };
-
-    const userBadges: PatientBadge[] = [];
-    const achievableBadges: Badge[] = await service.getAchievableBadges(userContext, userBadges);
-
-    // When
-    const goals = await service.generateGoals(patientId);
-    console.log('goals::', goals);
-
-    // Then
-    const goal1: Goal = {
-      patientId: '111',
-      name: 'Login in for 10 days in a row',
-      status: GoalStatus.COMPLETED,
-      rewards: [
-        {
-          id: '111',
-          dimension: 'patient',
-          metric: Metrics.PATIENT_STREAK,
-          minVal: 10,
-          maxVal: null,
-          name: 'Streak_10',
-          tier: 'bronze',
-          status: 'active',
-        },
-      ],
-    };
-    expect(goals).toContain(goal1);
   });
 });
